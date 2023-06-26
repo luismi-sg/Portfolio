14237a1dc4abadd5cfdc2e2b70396eb02e53a657
Luis Miguel SG <lmsg1191@gmail.com>
2023-06-27 01:15:17 +0200

    Update Contacto.jsx

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -9,7 +9,7 @@ export const Contacto = () => {
             <div className='Redes-wrapper'>
                 <ul className='Redes-ul'>
                     <li className='Redes-li'><a href='https://www.linkedin.com/in/luismisg/' target='_blank' title='Perfil de Linkedin'>LINKEDIN</a></li>
-                    <li className='Redes-li'><a href='https://github.com/luismi11' target='_blank' title='Perfil de Github'>GITHUB</a></li>
+                    <li className='Redes-li'><a href='https://github.com/luismi-sg' target='_blank' title='Perfil de Github'>GITHUB</a></li>
                     <li className='Redes-li'><a href='mailto:lmsg1191@gmail.com' target='_blank' title='Enviar un correo'>EMAIL</a></li>
                 </ul>
             </div>
