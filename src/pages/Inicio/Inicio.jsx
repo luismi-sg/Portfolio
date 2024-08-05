2eb8f953dfab750bcc770e8b298069fc38fc3bc8
Luis Miguel SG <lmsg1191@gmail.com>
2024-08-05 12:35:35 +0100

    Update Inicio.jsx

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -8,9 +8,9 @@ export const Inicio = () => {
                     <h1 className='Inicio-h1'> LuisMi </h1>
                     <h2 className='Inicio-h2'>FrontEnd Developer & UX/UI Designer</h2>
                 </main>
-                <footer>
+                {/* <footer>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores similique ea quis accusamus, veniam porro enim quidem obcaecati autem quam? Laboriosam sapiente incidunt in doloribus corporis deleniti, velit inventore nihil.</p>
-                </footer>
+                </footer> */}
             </div>
         </div>
     )
