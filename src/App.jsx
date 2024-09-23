8bb9c55635d0e1eee833aed609566c56147859c8
Luis Miguel SG <lmsg1191@gmail.com>
2024-09-23 17:24:45 +0100

    mobile menu changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,15 @@ import { WebWorks } from './pages/WebWorks/WebWorks'
 
 import { useTranslation } from 'react-i18next';
 import './i18n';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [isActive, setIsActive] = useState(false);
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
 
   const { t, i18n } = useTranslation();
   const changeLanguage = (lng) => {
@@ -28,7 +34,7 @@ function App() {
         <header className='Header'>
           <nav className='Header-nav'>
             <NavLink to={ '/' }>
-              <img className="Header-svg" src="/assets/Logo-luismi.svg" alt="Logo LuisMi Sovero" />
+              <img className="Header-svg" src="/assets/Logo-luismi.svg" alt="Luis Miguel Sovero Logo" />
             </NavLink>
             <ul className='Header-ul'>
               <NavLink to={ '/trabajos' }>  <li className='Header-li'>{t('projects')}</li>  </NavLink>
@@ -39,7 +45,35 @@ function App() {
               <button className='Header-button' onClick={() => changeLanguage('en')}>EN</button>
               <button className='Header-button' onClick={() => changeLanguage('es')}>ES</button>
             </div>
+            <div className="Toggle-menu" onClick={() => toggleMenu()}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+              </svg>
+            </div>
           </nav>
+          
+          { isActive && 
+            <div className="Header-nav mobile" onClick={() => toggleMenu()}>
+              <div className="Toggle-menu mobile" onClick={() => toggleMenu()}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg>
+              </div>
+              
+              <ul className='Header-ul-mobile'>
+                <NavLink to={ '/' }>
+                  <img className="Header-svg mobile" src="/assets/Logo-luismi.svg" alt="Luis Miguel Sovero Logo" />
+                </NavLink>
+                <NavLink to={ '/trabajos' }>  <li className='Header-li'>{t('projects')}</li>  </NavLink>
+                <NavLink to={ '/sobremi' }>   <li className='Header-li'>{t('about')}</li> </NavLink>
+                <NavLink to={ '/contacto' }>  <li className='Header-li'>{t('contact')}</li> </NavLink>
+              </ul>
+              <div className='Button-wrapper-mobile'>
+                <button className='Header-button' onClick={() => changeLanguage('en')}>EN</button>
+                <button className='Header-button' onClick={() => changeLanguage('es')}>ES</button>
+              </div>
+            </div>
+          }
         </header>
         
         
